ace99a7ffea199ae2e4f0c98e665a2851b904c26
Star Brilliant <m13253@hotmail.com>
2016-02-17 01:22:13 +0800

    Make some demo controls

--- demo/src/imp/main_window.cpp
+++ demo/src/imp/main_window.cpp
@@ -1,6 +1,14 @@
 #include "../main_window.h"
+#include <QCheckBox>
+#include <QComboBox>
+#include <QDial>
+#include <QDoubleSpinBox>
 #include <QLayout>
+#include <QLineEdit>
+#include <QProgressBar>
 #include <QPushButton>
+#include <QRadioButton>
+#include <QSlider>
 #include <QRockyStyle.h>
 
 MainWindow::MainWindow(QWidget *parent) :
@@ -16,12 +24,57 @@ MainWindow::MainWindow(QWidget *parent) :
     
     auto row0 = new QHBoxLayout;
     row0->setSpacing(qRound(em));
-    row0->addWidget(new QPushButton("OK"));
-    row0->addWidget(new QPushButton("Cancel"));
+    row0->addWidget(new QLineEdit("Input box"));
+    {
+        auto combo = new QComboBox;
+        combo->setEditable(true);
+        combo->addItem("44100");
+        combo->addItem("48000");
+        combo->addItem("96000");
+        combo->addItem("192000");
+        row0->addWidget(combo);
+    }
     layout->addItem(row0);
 
+    auto row1 = new QHBoxLayout;
+    row1->setSpacing(qRound(em));
+    row1->addWidget(new QDoubleSpinBox);
+    {
+        auto combo = new QComboBox;
+        combo->addItem("44100");
+        combo->addItem("48000");
+        combo->addItem("96000");
+        combo->addItem("192000");
+        row1->addWidget(combo);
+    }
+    layout->addItem(row1);
+
+    auto row2 = new QHBoxLayout;
+    row2->setSpacing(qRound(em));
+    row2->addWidget(new QSlider(Qt::Horizontal));
+    {
+        auto progbar = new QProgressBar;
+        progbar->setValue(42);
+        row2->addWidget(progbar);
+    }
+    layout->addItem(row2);
+
+    auto row3 = new QHBoxLayout;
+    row3->addWidget(new QDial);
+    auto row3col1 = new QVBoxLayout;
+    row3col1->addWidget(new QCheckBox("Check"));
+    row3col1->addWidget(new QRadioButton("Option 1"));
+    row3col1->addWidget(new QRadioButton("Option 2"));
+    row3->addItem(row3col1);
+    layout->addItem(row3);
+
+    auto row4 = new QHBoxLayout;
+    row4->setSpacing(qRound(em));
+    row4->addWidget(new QPushButton("OK"));
+    row4->addWidget(new QPushButton("Cancel"));
+    layout->addItem(row4);
+
     frame->setLayout(layout);
-    frame->setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Ignored);
     setCentralWidget(frame);
     adjustSize();
 }
