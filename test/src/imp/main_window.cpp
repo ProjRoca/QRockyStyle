8766e8bfbacc21943c2e948ff9203a65230e8d6e
Star Brilliant <m13253@hotmail.com>
2016-02-16 19:35:19 +0800

    Add some buttons

--- test/src/imp/main_window.cpp
+++ test/src/imp/main_window.cpp
@@ -1,7 +1,22 @@
 #include "../main_window.h"
+#include <QLayout>
+#include <QPushButton>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent) {
+
+    auto frame = new QWidget;
+    auto layout = new QVBoxLayout;
+    
+    auto row0 = new QHBoxLayout;
+    row0->addWidget(new QPushButton("OK"));
+    row0->addWidget(new QPushButton("Cancel"));
+    layout->addItem(row0);
+
+    frame->setLayout(layout);
+    frame->setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Ignored);
+    setCentralWidget(frame);
+    adjustSize();
 }
 
 MainWindow::~MainWindow() {
